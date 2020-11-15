448a0bd0dea42f04d1598af775bb5e462797d1d0
Ivan Fesenko <pharmivan@gmail.com>
2020-11-15 12:35:25 +0200

    init app

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Stats from "./Stats";
+
+function Profile(props) {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src="https://i.pinimg.com/originals/a0/40/66/a04066a2d1fcf25df39c599e093995c8.jpg"
+          alt="user avatar"
+          class="avatar"
+        />
+        <p class="name">Petra Marica</p>
+        <p class="tag">@pmarica</p>
+        <p class="location">Salvador, Brasil</p>
+      </div>
+
+      <Stats stats={props.stats} />
+    </div>
+  );
+}
+
+export default Profile;
